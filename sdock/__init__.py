872c646d2ebf5ae9d700c5397d959df9d6febcfc
franceme <frantzme@vt.edu>
2023-06-30 14:40:30 -0400

    updated installer

--- sdock/__init__.py
+++ sdock/__init__.py
@@ -705,22 +705,12 @@ end
 		self.destroy(emptyflag)
 
 
-def install_docker():
+def install_docker(save_file:bool=False):
 	#https://docs.docker.com/engine/install/ubuntu/
 	for string in [
-		"yes|sudo apt-get update",
-		"yes|sudo apt-get install ca-certificates curl gnupg",
-		"yes|sudo install -m 0755 -d /etc/apt/keyrings",
-		"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
-		"sudo chmod a+r /etc/apt/keyrings/docker.gpg",
-		"""echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-		""",
-		"sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-		"yes|sudo apt-get update",
-		"yes|sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
+		"curl -fsSL https://get.docker.com -o get-docker.sh",
+		"sudo sh ./get-docker.sh --dry-run",
+		'echo "Done"' if save_file else "echo \"Done\" && rm get-docker.sh"
 	]:
 		try:
 			cmd(string)
