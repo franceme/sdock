acc0748a2f72da22cc51949e6f6daa01d9d7ae8d
franceme <9311496+franceme@users.noreply.github.com>
2023-07-25 07:38:46 -0400

    trimmed up vagrant

--- sdock/vagrant.py
+++ sdock/vagrant.py
@@ -1,5 +1,4 @@
 import os, mystring
-from dataclasses import dataclass
 from datetime import datetime
 import vagrant as ogvag
 
@@ -7,7 +6,7 @@ import vagrant as ogvag
 # https://github.com/pycontribs/python-vagrant/tree/main
 
 
-class vagrant(ogvag.Vagrant): #https://github.com/pycontribs/python-vagrant/blob/main/src/vagrant/__init__.py#L202
+class vagrant(object): #ogvag.Vagrant #https://github.com/pycontribs/python-vagrant/blob/main/src/vagrant/__init__.py#L202
     def __init__(self,
         vagrant_base = "talisker/windows10pro",
         disablehosttime = True,
@@ -15,14 +14,14 @@ class vagrant(ogvag.Vagrant): #https://github.com/pycontribs/python-vagrant/blob
         vmdate = None,
         cpu = 2,
         ram = 4096,
-        uploadfiles = None,
-        choco_packages = None,
-        python_packages = None,
-        scripts_to_run = None,
+        uploadfiles = [],
+        choco_packages = [],
+        python_packages = [],
+        scripts_to_run = [],
         vb_path = None,
         vb_box_exe = "VBoxManage",
         headless = True,
-        save_files = None,
+        save_files = [],
     ):
         self.vagrant_base = vagrant_base #= "talisker/windows10pro",
         self.disablehosttime = disablehosttime # = True,
@@ -39,69 +38,45 @@ class vagrant(ogvag.Vagrant): #https://github.com/pycontribs/python-vagrant/blob
         self.headless = headless # = True
         self.save_files = save_files # = None
 
-    def __post_init__(self):
-        if self.uploadfiles is None or type(self.uploadfiles) is tuple:
-            self.uploadfiles = []
-
-        if self.choco_packages is None or type(self.choco_packages) is tuple:
-            self.choco_packages = []
-
-        if self.python_packages is None or type(self.python_packages) is tuple:
-            self.python_packages = []
-
-        if self.scripts_to_run is None or type(self.scripts_to_run) is tuple:
-            self.scripts_to_run = []
-
-        if self.save_files is None or type(self.save_files) is tuple:
-            self.save_files = []
-
-        if self.vmdate is None or type(self.vmdate) is tuple:
-            self.vmdate = None
-
     @property
     def vagrant_name(self):
         if not self.vb_path:
             return
 
-        vag_name = None
-
-        folder_name = os.path.basename(os.path.abspath(os.curdir))
+        folder_name,vag_name = os.path.basename(os.path.abspath(os.curdir)),None
         for item in os.listdir(self.vb_path):
             if not os.path.isfile(item) and folder_name in item:
                 vag_name = item.split('/')[-1].strip()
 
         return vag_name
+    
+    def __snapshot_prep(self):
+        return "{0} snapshot {1}".format(self.vboxmanage, self.vagrant_name)
 
     def snapshot_take(self, snapshotname):
-        vb_name = self.vagrant_name
         # https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-snapshot.html
-        mystring.string("{0} snapshot {1} take {2}".format(self.vboxmanage, vb_name, snapshotname)).exec()
+        mystring.string("{0} take {1}".format(self.__snapshot_prep(), snapshotname)).exec()
 
     def snapshot_load(self, snapshotname):
-        vb_name = self.vagrant_name
         # https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-snapshot.html
-        mystring.string("{0} snapshot {1} restore {2}".format(self.vboxmanage, vb_name, snapshotname)).exec()
+        mystring.string("{0} restore {1}".format(self.__snapshot_prep(), snapshotname)).exec()
 
     def snapshot_list(self):
-        vb_name = self.vagrant_name
         # https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-snapshot.html
-        mystring.string("{0} snapshot {1} list".format(self.vboxmanage, vb_name)).exec()
+        mystring.string("{0} list".format(self.__snapshot_prep())).exec()
 
     def snapshot_delete(self, snapshotname):
-        vb_name = self.vagrant_name
         # https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-snapshot.html
-        mystring.string("{0} snapshot {1} delete {2}".format(self.vboxmanage, vb_name, snapshotname)).exec()
+        mystring.string("{0} delete {1}".format(self.__snapshot_prep(), snapshotname)).exec()
 
     def export_to_ova(self, ovaname):
-        vb_name = self.vagrant_name
         # https://www.techrepublic.com/article/how-to-import-and-export-virtualbox-appliances-from-the-command-line/
         # https://docs.oracle.com/en/virtualization/virtualbox/6.0/user/vboxmanage-export.html
-        mystring.string("{0} export {1} --ovf10 --options manifest,iso,nomacs -o {2}".format(self.vboxmanage, vb_name,
-                                                                                             ovaname)).exec()
+        mystring.string("{0} export {1} --ovf10 --options manifest,iso,nomacs -o {2}".format(self.__snapshot_prep(),ovaname)).exec()
 
-    # https://jd-bots.com/2021/05/15/how-to-run-powershell-script-on-windows-startup/
-    # https://stackoverflow.com/questions/20575257/how-do-i-run-a-powershell-script-when-the-computer-starts
     def create_runner(self):
+        # https://jd-bots.com/2021/05/15/how-to-run-powershell-script-on-windows-startup/
+        # https://stackoverflow.com/questions/20575257/how-do-i-run-a-powershell-script-when-the-computer-starts
         with open("on_login.cmd", "w+") as writer:
             writer.write("""powershell -windowstyle hidden C:\\\\Users\\\\vagrant\\\\Desktop\\\\on_start.ps1""")
         return "on_login.cmd"
@@ -122,9 +97,7 @@ class vagrant(ogvag.Vagrant): #https://github.com/pycontribs/python-vagrant/blob
         with open("on_start.ps1", "w+") as writer:
             writer.write("""
 {0}
-""".format(
-                "\n".join(contents)
-            ))
+""".format("\n".join(contents)))
         return "on_start.ps1"
 
     def add_file(self, foil, directory="C:\\\\Users\\\\vagrant\\\\Desktop"):
@@ -134,27 +107,23 @@ class vagrant(ogvag.Vagrant): #https://github.com/pycontribs/python-vagrant/blob
         self.uploadfiles = list(self.uploadfiles)
         self.uploadfiles += [self.write_startup_file()]
         uploading_file_strings = []
+
         for foil in self.uploadfiles:
             uploading_file_strings += [self.add_file(foil)]
 
         uploading_file_strings += [
-            self.add_file(self.create_runner(),
-                          """C:\\\\Users\\\\vagrant\\\\AppData\\\\Roaming\\\\Microsoft\\\\Windows\\\\Start Menu\\\\Programs\\\\Startup""")
+            self.add_file(self.create_runner(),"""C:\\\\Users\\\\vagrant\\\\AppData\\\\Roaming\\\\Microsoft\\\\Windows\\\\Start Menu\\\\Programs\\\\Startup""")
         ]
 
         scripts = []
         for script in self.scripts_to_run:
             if script:
-                scripts += [
-                    """win10.vm.provision "shell", inline: <<-SHELL
+                scripts += ["""win10.vm.provision "shell", inline: <<-SHELL
 {0}
-SHELL""".format(script)
-                ]
+SHELL""".format(script)]
 
         if self.python_packages != []:
-            self.choco_packages += [
-                "python38"
-            ]
+            self.choco_packages += ["python38"]
 
         if self.choco_packages:
             choco_script = """win10.vm.provision "shell", inline: <<-SHELL
@@ -219,8 +188,7 @@ end
             if self.vmdate:
                 diff_days = (self.vmdate - datetime.now().date())
                 ms = round(diff_days.total_seconds() * 1000)
-                mystring.string(
-                    "{0} modifyvm {1} --biossystemtimeoffset {2}".format(self.vb_box_exe, self.vagrant_name, ms)).exec()
+                mystring.string("{0} modifyvm {1} --biossystemtimeoffset {2}".format(self.vb_box_exe, self.vagrant_name, ms)).exec()
 
             cmd = "{0} startvm {1}".format(self.vb_box_exe, self.vagrant_name)
             if self.headless:
@@ -231,11 +199,9 @@ end
             print("Vagrant VM hasn't been created yet")
 
     def off(self):
-        self.vagrant_name
         mystring.string("{0} controlvm {1} poweroff".format(self.vb_box_exe, self.vagrant_name)).exec()
 
-    def destroy(self, emptyflag=False):
-        self.vagrant_name
+    def destroy(self):
         mystring.string(""" vagrant destroy -f """).exec()
         for foil in ["Vagrant", "on_start*", "on_login*"]:
             mystring.string("rm {0}".format(foil)).exec()
