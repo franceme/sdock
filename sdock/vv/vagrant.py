2411009f59a11f92fbd72ac014600a145bc418d7
franceme <frantzme@vt.edu>
2024-01-07 20:29:50 -0500

    re

--- sdock/vv/vagrant.py
+++ sdock/vv/vagrant.py
@@ -56,49 +56,6 @@ delete:
 	VBoxManage unregistervm --delete "$$name"
 
 # How this works with the file
-#!/usr/bin/env python3
-import os,sys
-from importlib.metadata import version
-from datetime import datetime
-
-try:
-	if version('sdock') < '0.1.77':
-		raise Exception("Upgrade the version")
-except:
-	os.system("{0} -m pip install --upgrade sdock".format(sys.executable))
-from sdock.vv import *
-
-#Choco Packages: https://community.chocolatey.org/packages
-
-box_name = "tempbox"
-box = vagrant.app(
-	box="talisker/windows10pro",
-	name=box_name,
-	provider=virtualbox.app(),
-	disablehosttime = True,
-	disablenetwork = True,
-	vmdate = datetime(year=2023, month=1, day=10, hour=3, minute=0, second=0),
-	python_packages = ["hugg"],
-)
-
-def getArgs():
-	import argparse
-	parser = argparse.ArgumentParser("Vagrant Runner for {0}".format(box_name))
-	parser.add_argument("--start", help="Start the box",action='store_true',default=False)
-	parser.add_argument("--stop", help="Stop the box",action='store_true',default=False)
-	parser.add_argument("--clean", help="Clean the box",action='store_true',default=False)
-	#parser.add_argument("--save", help="Save the progress with the box",action='store_true',default=False)
-	#parser.add_argument("--load", help="Load the progress with the box",action='store_true',default=False)
-	args,unknown = parser.parse_known_args()
-	return args
-
-args = getArgs()
-if args.start:
-	box.on()
-elif args.stop:
-	box.off()
-elif args.clean:
-	box.clean()
 		"""
 		name:str
 		provider:Provider
@@ -188,7 +145,7 @@ elif args.clean:
 			else:
 				return """win10.vm.provision "shell", inline: <<-SHELL\n{0}\nSHELL""".format("\n".join(contents))
 
-		def choco_packages(self):
+		def prep_choco_packages(self):
 			return shell_wrap(
 				"""[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" """,
 				"""iex (wget 'https://chocolatey.org/install.ps1' -UseBasicParsing)""",
@@ -252,7 +209,7 @@ end
 	name=self.name,
 	box=self.box,
 	shell_scripts="\n		".join(shell_scripts)),
-	choco_script=self.choco_packages()
+	choco_script=self.prep_choco_packages()
 )
 			return foil
 
